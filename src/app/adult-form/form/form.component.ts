4ee101d4a2ef479599fdd1e7ad84bbd583d0a0eb
Servio Pantoja <servio.andres.pantoja@gmail.com>
2018-09-16 12:15:15 -0500

    Format code and full responsive

--- src/app/adult-form/form/form.component.ts
+++ src/app/adult-form/form/form.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// Import the User model
 import { UserModel } from '../adultModel';
 
 @Component({
@@ -9,29 +8,27 @@ import { UserModel } from '../adultModel';
 })
 export class AdultFormComponent implements OnInit {
 
-    //Property for the gender
-    private gender: string[];
-    //Property for the user
-    private user:UserModel;
+  private gender: string[];
+  private user: UserModel;
 
   constructor() { }
 
   ngOnInit() {
-    this.gender =  ['Male', 'Female', 'Others'];
+    this.gender = ['Male', 'Female', 'Others'];
     //Create a new user object
-    this.user = new UserModel({ fullName: "", lastName:"", dateOfBirth:"",
-        email:"", password: { pwd: "" , confirm_pwd: ""}, 
-        gender: this.gender[0], terms: false});
+    this.user = new UserModel({
+      fullName: "", lastName: "", dateOfBirth: "",
+      email: "", password: { pwd: "", confirm_pwd: "" },
+      gender: this.gender[0], terms: false
+    });
   }
 
   log(object) {
-  	console.log(object);
+    console.log(object);
   }
 
-   onFormSubmit({ value, valid}: { value: UserModel, valid: boolean }) {
-    	this.user = value;
-    	console.log( this.user);
-    	console.log("valid: " + valid);
-  	}
+  onFormSubmit({ value, valid }: { value: UserModel, valid: boolean }) {
+    this.user = value;
+  }
 
 }
