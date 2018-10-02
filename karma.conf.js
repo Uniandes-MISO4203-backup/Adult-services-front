24d5e62734f6d1b34b56833e89a54cdab711c0f3
Yanerys De La Hoz Maestre <42595999+yanerys@users.noreply.github.com>
2018-10-01 23:03:48 -0500

    Probando funcionamiento del karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-require('phantomjs-prebuilt').path = './node_modules/.bin/phantomjs';
+//require('phantomjs-prebuilt').path = './node_modules/.bin/phantomjs';
 
 module.exports = function (config) {
     config.set({
