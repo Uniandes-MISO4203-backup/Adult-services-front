e62e281eed9ce824dbc3866be93565df29f73850
Yanerys De La Hoz Maestre <yi.de10@uniandes.edu.co>
2018-11-01 21:51:01 -0500

    prueba

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
+// Serve only the static files form the dist directory/
 app.use(express.static('./dist'));
 
 app.get('/*', function(req,res) {
